3188703b7256fb5e251b1d8c427ea7a598f2a3f9
ynr-cs <001400007244qrb4@gl.pen-kanagawa.ed.jp>
2026-09-11 06:02:21 +0900

    0.5.304

--- main/data/updates.js
+++ main/data/updates.js
@@ -1,14 +1,8 @@
 export const updatesList = [
   {
-    id: "agile-development",
-    title: "変化を恐れない。アジャイルで創る新しい南陵祭",
-    file: "agile-development.md",
-    badge: "ENGINEERING"
-  },
-  {
-    id: "make-nanryo-great-again",
-    title: "Make Nanryo Great Again",
-    file: "make-nanryo-great-again.md",
-    badge: "VISION"
+    id: "mobile-order",
+    title: "モバイルオーダーについて",
+    file: "mobile-order-vision.md",
+    badge: "FEATURE"
   }
 ];
